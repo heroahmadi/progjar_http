1f4e5b7c332c61f94cb1851255df151e6bbfd12f
Unknown <hudafauzanm@gmail.com>
2017-12-14 19:08:46 +0700

    Delete Folder Selesai Nomer 6

--- cobahttpserver/ThreadServerHTTPServer.py
+++ cobahttpserver/ThreadServerHTTPServer.py
@@ -8,6 +8,7 @@ import mimetypes
 import re
 import zipfile
 import cgitb
+
 cgitb.enable()
 try:
     from cStringIO import StringIO
@@ -23,53 +24,65 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             f.close()
 
     def do_HEAD(self):
-        
+
         f = self.send_head()
         if f:
             f.close()
 
     def do_POST(self):
         """Serve a POST request."""
-	ctype,pdict = cgi.parse_header(self.headers.getheader('Content-type'))
+        ctype, pdict = cgi.parse_header(self.headers.getheader('Content-type'))
         if ctype == 'multipart/form-data':
-		print "file"
-		#query = cgi.parse_multipart(self.rfile, pdict)
-	        r, info = self.deal_post_data()
-		print r, info, "by: ", self.client_address
-		f = StringIO()
-		f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
-		f.write("<html>\n<title>Upload Result Page</title>\n")
-		f.write("<body>\n<h2>Upload Result Page</h2>\n")
-		f.write("<hr>\n")
-		if r:
-		    f.write("<strong>Success:</strong>")
-		else:
-		    f.write("<strong>Failed:</strong>")
-		f.write(info)
-		f.write("<br><a href=\"%s\">back</a>" % self.headers['referer'])
-		f.write("<hr><small>Powerd By: bones7456, check new version at ")
-		f.write("<a href=\"http://li2z.cn/?s=SimpleHTTPServerWithUpload\">")
-		f.write("here</a>.</small></body>\n</html>\n")
-		length = f.tell()
-		f.seek(0)
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.send_header("Content-Length", str(length))
-		self.end_headers()
-		if f:
-		    self.copyfile(f, self.wfile)
-		    f.close()
-	else:
-		length = int(self.headers['content-length'])
-                postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
-		if postvars.get('hapusdir') is not None:
-			print "Hapus Directory"			
-			print postvars['hapusdir']
-		elif postvars.get('hapusfile') is not None:
-			print "Hapus File"			
-			print postvars['hapusfile']
-		
-        
+            print "file"
+            # query = cgi.parse_multipart(self.rfile, pdict)
+            r, info = self.deal_post_data()
+            print r, info, "by: ", self.client_address
+            f = StringIO()
+            f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+            f.write("<html>\n<title>Upload Result Page</title>\n")
+            f.write("<body>\n<h2>Upload Result Page</h2>\n")
+            f.write("<hr>\n")
+            if r:
+                f.write("<strong>Success:</strong>")
+            else:
+                f.write("<strong>Failed:</strong>")
+            f.write(info)
+            f.write("<br><a href=\"%s\">back</a>" % self.headers['referer'])
+            f.write("<hr><small>Powerd By: bones7456, check new version at ")
+            f.write("<a href=\"http://li2z.cn/?s=SimpleHTTPServerWithUpload\">")
+            f.write("here</a>.</small></body>\n</html>\n")
+            length = f.tell()
+            f.seek(0)
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.send_header("Content-Length", str(length))
+            self.end_headers()
+            if f:
+                self.copyfile(f, self.wfile)
+                f.close()
+        else:
+            length = int(self.headers['content-length'])
+            postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+            if postvars.get('hapusdir') is not None:
+                s=os.getcwd()
+                d=postvars['hapusdir']
+
+                d=str(d)
+                d=d.replace("[","")
+                d=d.replace("]","")
+                d=d.replace("'","")
+                print d
+                s=s+"\\"+d
+                print s
+                os.rmdir(s)
+                print "Hapus Directory"
+                print postvars['hapusdir']
+
+            elif postvars.get('hapusfile') is not None:
+                print "Hapus File"
+                print postvars['hapusfile']
+
+
     def deal_post_data(self):
         boundary = self.headers.plisttext.split("=")[1]
         remainbytes = int(self.headers['content-length'])
@@ -92,7 +105,7 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             out = open(fn, 'wb')
         except IOError:
             return (False, "Can't create file to write, do you have permission to write?")
-                
+
         preline = self.rfile.readline()
         remainbytes -= len(preline)
         while remainbytes > 0:
@@ -110,23 +123,24 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 preline = line
         return (False, "Unexpect Ends of data.")
 
+
     def send_head(self):
         path = self.translate_path(self.path)
         f = None
-	print path
-	if self.path.endswith('?download'):
-		tmp_file = "tmp.zip"
-        	self.path = self.path.replace("?download","")
+        print path
+        if self.path.endswith('?download'):
+            tmp_file = "tmp.zip"
+            self.path = self.path.replace("?download", "")
 
-        	zip = zipfile.ZipFile(tmp_file, 'w')
-        	for root, dirs, files in os.walk(path):
-			for file in files:
-		                if os.path.join(root, file) != os.path.join(root, tmp_file):
-					zip.write(os.path.join(root, file))
-		zip.close()
-		path = self.translate_path(tmp_file)
+            zip = zipfile.ZipFile(tmp_file, 'w')
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    if os.path.join(root, file) != os.path.join(root, tmp_file):
+                        zip.write(os.path.join(root, file))
+            zip.close()
+            path = self.translate_path(tmp_file)
         elif os.path.isdir(path):
-	    print "Benar"
+            print "Benar"
             if not self.path.endswith('/'):
                 # redirect browser - doing basically what apache does
                 self.send_response(301)
@@ -157,8 +171,8 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.end_headers()
         return f
 
+
     def list_directory(self, path):
-        
         try:
             list = os.listdir(path)
         except os.error:
@@ -174,18 +188,18 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         f.write("<form ENCTYPE=\"multipart/form-data\" method=\"post\">")
         f.write("<input name=\"file\" type=\"file\"/>")
         f.write("<input type=\"submit\" value=\"upload\"/></form>\n")
-	f.write("<hr>\n")
-	f.write("<h2>Hapus Directory</h2>")
-	f.write("<form method=\"post\">")
-	f.write("<input name=\"hapusdir\"/>")
-	f.write("</form>")
-	f.write("<hr>\n")
-	f.write("<h2>Hapus File</h2>")
-	f.write("<form method=\"post\">")
-	f.write("<input name=\"hapusfile\"/>")
-	f.write("</form>")
-	f.write("<hr>\n")
-	f.write("<a href='%s'>%s</a>\n" % (self.path+"?download",'Download Directory Tree as Zip'))
+        f.write("<hr>\n")
+        f.write("<h2>Hapus Directory</h2>")
+        f.write("<form method=\"post\">")
+        f.write("<input name=\"hapusdir\"/>")
+        f.write("</form>")
+        f.write("<hr>\n")
+        f.write("<h2>Hapus File</h2>")
+        f.write("<form method=\"post\">")
+        f.write("<input name=\"hapusfile\"/>")
+        f.write("</form>")
+        f.write("<hr>\n")
+        f.write("<a href='%s'>%s</a>\n" % (self.path + "?download", 'Download Directory Tree as Zip'))
         f.write("<hr>\n<ul>\n")
         for name in list:
             fullname = os.path.join(path, name)
@@ -208,9 +222,10 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.end_headers()
         return f
 
+
     def translate_path(self, path):
-        path = path.split('?',1)[0]
-        path = path.split('#',1)[0]
+        path = path.split('?', 1)[0]
+        path = path.split('#', 1)[0]
         path = posixpath.normpath(urllib.unquote(path))
         words = path.split('/')
         words = filter(None, words)
@@ -222,8 +237,10 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             path = os.path.join(path, word)
         return path
 
+
     def copyfile(self, source, outputfile):
         shutil.copyfileobj(source, outputfile)
+
 
     def guess_type(self, path):
         base, ext = posixpath.splitext(path)
@@ -235,20 +252,21 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         else:
             return self.extensions_map['']
 
+
     if not mimetypes.inited:
-        mimetypes.init() # try to read system mime.types
+        mimetypes.init()  # try to read system mime.types
     extensions_map = mimetypes.types_map.copy()
     extensions_map.update({
-        '': 'application/octet-stream', # Default
+        '': 'application/octet-stream',  # Default
         '.py': 'text/plain',
         '.c': 'text/plain',
         '.h': 'text/plain',
-        })
+    })
 
 
-def main(HandlerClass = SimpleHTTPRequestHandler,ServerClass = BaseHTTPServer.HTTPServer):
+def main(HandlerClass=SimpleHTTPRequestHandler, ServerClass=BaseHTTPServer.HTTPServer):
     BaseHTTPServer.test(HandlerClass, ServerClass)
+
 
 if __name__ == '__main__':
     main()
-
