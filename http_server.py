27f3824d7d08db1e092c6ce239e9d3e93d769cb3
djohanp <djohan.prabowo1927@gmail.com>
2017-12-18 15:55:36 +0700

    Akhirnya Upload Gambar, PDF

--- http_server.py
+++ http_server.py
@@ -142,34 +142,30 @@ def response_no2():
 	return hasil
 
 def response_input_no2(req):
-	#formData = cgi.FieldStorage()
-	#print formData
 	a,req=req.split("name=\"fileToUpload\"; ")
-	#req,a,c=req.split(" -----------------------------")
-	req=req.split("-----------------------------")	
-	b=req[0].split("Content-Type: ")
-	#get isi
-	x=b[1].split("\n\r")
-	print len(x)
-	print "menghilangkan content type"
-	print x
-	#menciptakan judul
+	print "=======================Debug req===================="
+	req=req.split("\r\n\r\n")
+	#print req[0]
+	flnm=req[0].split("Content-Type")
+	flnm=flnm[0].split("filename=\"")
+	flnm=flnm[1].split("\"")
+	flnm=flnm[0]
+	print flnm
+	print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	isi=req[1].split("\n-----------------------------")
+	isi=isi[0]
+	print isi
+	print "=======================Akhir debug=================="
 	now = datetime.datetime.now()
 	now = str(now)
 	now = now.replace(".","")
 	now = now.replace(" ","")
-	a=b[0].split("Content-Type: ")
-	flnm=a[0].split("filename=\"")
-	flnm=flnm=flnm[1].split("\"")
-	flnm=now+flnm[0]
+	flnm=now+flnm
+	flnm=str(flnm)
 	print flnm
-	file = open(flnm,"w")
-	for xy in range(len(x)):
-		if xy!=0:
-			print "print"
-			file.write(x[xy])
-	file.close()
-	#b[1]isi file
+	f = open(flnm,"wb+")
+	f.write(isi)
+	f.close()
 	msg="Sukses"
 	panjang = len(msg)
 	hasil = "HTTP/1.1 200 OK\r\n" \
@@ -257,8 +253,7 @@ def pindah(url, data):
 		"\r\n" \
 		"{}" . format(panjang, data)
 	return hasil
-	
-"""
+
 def response_no8():
 	mydir= ("<input type=\"text\" name=\"input\" id=\"folder\" placeholder=\"Masukkan Folder yang akan dihapus\" /> <input type=\"submit\" value=\"submit\"/> ")
 	panjang = len(mydir)
@@ -274,7 +269,7 @@ def response_no8():
 		"\r\n" \
 		"{}" . format(panjang, mydir)
 	return hasil
-"""
+
 
 def response_gambar():
 	filegambar = open('gambar.png','r').read()
@@ -315,41 +310,6 @@ def response_redirect():
 
 #fungsi melayani client
 def layani_client(koneksi_client,alamat_client):
-<<<<<<< HEAD
-    try:
-       print >>sys.stderr, 'ada koneksi dari ', alamat_client
-       request_message = ''
-       while True:
-           data = koneksi_client.recv(64)
-	   data = bytes.decode(data)
-           request_message = request_message+data
-	   if (request_message[-4:]=="\r\n\r\n"):
-		break
-
-       baris = request_message.split("\r\n")
-       baris_request = baris[0]
-       print baris_request
- 	
-       a,url,c = baris_request.split(" ")
-       
-       if (url=='/favicon.ico'):
-          respon = response_icon()
-       elif (url=='/doc'):
-	  respon = response_dokumen()
-       elif (url=='/teks'):
-          respon = response_teks()
-       elif (url=='/1'):
-		  respon = response_no1()
-       elif (url=='/7'):
-		  respon = response_no7()
-       else:
-          respon = response_gambar()
-
-       koneksi_client.send(respon)
-    finally:
-        # Clean up the connection
-        koneksi_client.close()
-=======
 	try:
 		print >>sys.stderr, 'ada koneksi dari ', alamat_client
 		request_message = ''
@@ -369,13 +329,14 @@ def layani_client(koneksi_client,alamat_client):
 					break
 		elif("POST" in data):
 			print "Method Post"
+			msg=bytearray("")
 			while True:
 				data = koneksi_client.recv(4096)
-				request_message = request_message+data
+				msg = msg+data
 				if ("countryxsz" in data):
 					break
 		
-		print request_message
+		#print request_message
 		baris = request_message.split("\r\n")
 		baris_request = baris[0]
 		print baris_request		
@@ -410,11 +371,12 @@ def layani_client(koneksi_client,alamat_client):
 		elif ("POST" in a and url=='/input'):
 			#formData = cgi.FieldStorage()
 			#name=formData.getvalue('name_field')
-			#print "xxxx"
-			#print name
+			#print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx--msg mulai--xx"
+			#print msg
 			#print formData
-			#print "xxxx"
-			respon = response_input_no2(request_message)
+			#print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx---msg akhir--xx"
+			
+			respon = response_input_no2(msg)
 		elif (url=='/6'):
 			respon = response_no6()
 		elif (url=='/8'):
@@ -426,7 +388,6 @@ def layani_client(koneksi_client,alamat_client):
 	finally:
 		# Clean up the connection
 		koneksi_client.close()
->>>>>>> master
 
 
 while True:
